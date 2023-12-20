c1ec9d7a1cef90118b15570eaf90b2a84e368383
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-20 13:14:47 -0300

    adicionando algumas validaçoes no formulario e mensagem de sucess

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,7 +1,8 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../../services/courses.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
@@ -12,13 +13,14 @@ export class CourseFormComponent {
   form: FormGroup;
 
   constructor(
+    private location: Location,
+    private snackBar: MatSnackBar,
     private formBuilder: FormBuilder,
-    private coursesService: CoursesService,
-    private snackBar: MatSnackBar
+    private coursesService: CoursesService
   ) {
     this.form = this.formBuilder.group({
-      name: [null],
-      category: [null],
+      name: [null, [Validators.required, Validators.maxLength(150)]],
+      category: [null, Validators.required],
     });
   }
 
@@ -27,10 +29,23 @@ export class CourseFormComponent {
     if (this.form.valid) {
       this.coursesService.saveCourse(this.form.value).subscribe({
         // Usando a {} com next e error para tratamento correto
-        next: (data) => console.log(data),
+        next: () => this.onSuccess(),
         error: () => this.onError(),
       });
+    } else {
+      this.form.markAllAsTouched();
+      this.form.updateValueAndValidity();
     }
+  }
+
+  onCancel(): void {
+    this.form.reset();
+    this.location.back();
+  }
+
+  private onSuccess(): void {
+    this.snackBar.open('Curso criado com sucesso!', '', { duration: 3000 });
+    this.onCancel();
   }
 
   private onError(): void {
