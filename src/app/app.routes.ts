1de33e0184262a775e02423f98ef55b6f33b9917
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-30 12:28:29 -0300

    adicionando pagina de ERRO

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,14 @@
 import { Routes } from '@angular/router';
+import { ErrorPageComponent } from './courses/container/error-page/error-page.component';
 
 export const APP_ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  { path: 'error', component: ErrorPageComponent },
   {
     path: 'courses',
     loadChildren: () =>
       import('./courses/courses.routes').then(
         (module) => module.COURSES_ROUTES
       ),
-  },
-  {
-    path: '**',
-    redirectTo: 'courses',
   },
 ];
--- src/app/courses/courses.routes.ts
+++ src/app/courses/courses.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { courseResolver } from './guard/course.resolver';
 import { CoursesComponent } from './container/courses/courses.component';
+import { ErrorPageComponent } from './container/error-page/error-page.component';
 import { CourseFormComponent } from './container/course-form/course-form.component';
 
 export const COURSES_ROUTES: Routes = [
-  { path: '', component: CoursesComponent },
+  { path: '', pathMatch: 'full', component: CoursesComponent },
+  { path: 'error', component: ErrorPageComponent },
   {
     path: 'new',
     component: CourseFormComponent,
